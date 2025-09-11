dca2b52a62617678459f07cf618870a83d94e01e
Raunick Vileforte <raunickbhdesign@gmail.com>
2025-09-11 11:11:11 -0300

    Initial Commit

---
+++ Escalas-Hospitalares-ios26/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Escalas-Hospitalares-ios26
+//
+//  Created by Raunick Vileforte Vieira Generoso on 11/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Escalas-Hospitalares-ios26/Escalas_Hospitalares_ios26App.swift
@@ -0,0 +1,17 @@
+//
+//  Escalas_Hospitalares_ios26App.swift
+//  Escalas-Hospitalares-ios26
+//
+//  Created by Raunick Vileforte Vieira Generoso on 11/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Escalas_Hospitalares_ios26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
